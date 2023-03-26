3ec722dbd82486e26a71177a29a590f855598874
Huy Dang <huy.dang110902@gmail.com>
2023-03-26 21:33:46 +0700

    Final for me

--- level.py
+++ level.py
@@ -22,15 +22,3 @@ class Level:
         self.o_btn = map_bloxoz.get('o_btn')
         self.split_btn = map_bloxoz.get('split_btn')
     
-#Check for the Load of Level
-if __name__ == '__main__':
-    for i in range (1,34):
-        level = Level(i)
-        print('Level',f'{level.level}')
-        print('Start:',level.start)
-        print('Goal :',level.goal)
-        print('Fragile:',level.fragile_cells)
-        print('X Button List:',level.x_btn)
-        print('O Button List:',level.o_btn)    
-        print('Split Btton List:',level.split_btn)
-        print('Map:',level.board)
