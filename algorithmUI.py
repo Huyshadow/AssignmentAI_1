2c404a7d7d99b7b94a23afaeca269938ec9397c4
Huy Dang <huy.dang110902@gmail.com>
2023-03-28 00:15:30 +0700

    Check out

--- algorithmUI.py
+++ algorithmUI.py
@@ -229,9 +229,3 @@ class AlgorithmUI_Show:
     def should_quit(self):
         return self.ESC
     
-
-if __name__ == '__main__':
-    # with open('results/ga.txt','w') as f:
-    background = pygame.display.set_mode((900,770))
-    p1 = AlgorithUI_Stats(background, 770,900,1,'BFS')
-    
--- level.py
+++ level.py
@@ -8,17 +8,17 @@ class Level:
     
     def runLevel(self,path_level):
         with open(path_level,'r') as file_map:
-            map_bloxoz = json.load(file_map)
-        self.board = num.asarray(map_bloxoz.get('map'), dtype=bool)
+            map_blo = json.load(file_map)
+        self.board = num.asarray(map_blo.get('map'), dtype=bool)
         self.size_y, self.size_x = self.board.shape
         
-        self.start = map_bloxoz.get('start')
-        self.goal = map_bloxoz.get('end')
+        self.start = map_blo.get('start')
+        self.goal = map_blo.get('end')
 
-        tmp_board = num.asarray(map_bloxoz.get('map'), dtype=int)
+        tmp_board = num.asarray(map_blo.get('map'), dtype=int)
         self.fragile_cells = [(r,c) for r,c in zip(*num.where(tmp_board == 2))]
 
-        self.x_btn = map_bloxoz.get('x_btn')
-        self.o_btn = map_bloxoz.get('o_btn')
-        self.split_btn = map_bloxoz.get('split_btn')
+        self.x_btn = map_blo.get('x_btn')
+        self.o_btn = map_blo.get('o_btn')
+        self.split_btn = map_blo.get('split_btn')
     
